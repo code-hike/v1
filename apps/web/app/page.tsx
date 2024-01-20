95e12502c67d08a4d1a29fbf5fed068324a9e847
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-01-20 11:45:40 +0100

    Landing

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,17 @@
 import { Card, Cards } from "next-docs-ui/mdx/card"
 import Link from "next/link"
-import { AllSponsors } from "./landing/sponsors"
+import { AllSponsors, PoweredBy } from "./landing/sponsors"
 
 export default function HomePage() {
   return (
-    <main className="min-h-screen max-w-3xl mx-auto mb-24">
+    <main className="min-h-screen max-w-3xl mx-auto">
       <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white pt-28  max-w-2xl mx-auto">
         Superpowered
         <br /> Markdown
       </h1>
       <h2 className="mt-6 text-xl text-primary/80 text-center max-w-3xl mx-auto  pb-16">
-        The authoring experience of Markdown meets the power of React
+        The authoring experience of Markdown meets the dynamic versatility of
+        React
       </h2>
 
       <div className="flex w-full justify-center gap-4 pb-16">
@@ -28,7 +29,13 @@ export default function HomePage() {
         </Link>
       </div>
 
-      <AllSponsors />
+      <div className="w-full h-96 bg-primary/10 flex justify-center items-center rounded-lg mb-16">
+        Code Hike Visualization
+      </div>
+
+      <AllSponsors className="mb-24" />
+
+      <PoweredBy className="mb-8 text-center flex items-center justify-center gap-4 w-full" />
     </main>
   )
 }
