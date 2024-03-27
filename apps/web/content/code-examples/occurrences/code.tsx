938f19fe3e1c980aff334f253470f432cbf6f129
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-03-27 11:24:56 +0100

    Add future examples

--- apps/web/content/code-examples/occurrences/code.tsx
+++ apps/web/content/code-examples/occurrences/code.tsx
@@ -42,9 +42,7 @@ export function CodeWithOccurrences({
     <CodeRender
       ref={ref}
       info={info}
-      components={{
-        Token,
-      }}
+      components={{ Token }}
     />
   )
 }
