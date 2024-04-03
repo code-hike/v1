dd2829080f8be13c9937f37d5ca6f56c8dfb8b10
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-04-03 18:39:38 +0200

    Fold

--- apps/web/components/demo.tsx
+++ apps/web/components/demo.tsx
@@ -27,7 +27,7 @@ export async function Demo({
   )
 
   const usage = (
-    <div className="border border-zinc-700 rounded overflow-hidden">
+    <div className="border border-zinc-700 rounded overflow-hidden min-h-full">
       <div className="border-b border-zinc-700 bg-zinc-900 px-3 py-2 text-zinc-300 text-sm flex">
         content.md
         <CopyButton text={highlighted.code} className="ml-auto" />
@@ -50,7 +50,7 @@ export async function Demo({
   )
 
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 items-stretch">
       <div className="min-w-0 flex-1">{usage}</div>
       {preview}
     </div>
