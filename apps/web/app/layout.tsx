07315bd803827730c94986621c2043f0ea53cd80
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-04-09 10:28:18 +0200

    Add analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import { RootProvider } from "next-docs-ui/provider"
 import { Inter } from "next/font/google"
 import type { ReactNode } from "react"
 import { NavBar } from "../ui/nav"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,6 +17,7 @@ export default function Layout({ children }: { children: ReactNode }) {
           <NavBar />
           {children}
         </RootProvider>
+        <Analytics />
       </body>
     </html>
   )
