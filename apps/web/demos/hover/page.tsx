9378e09f0511583511ad55b49859dc437c297904
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-07-27 12:28:22 +0200

    Fix typescript

--- apps/web/demos/hover/page.tsx
+++ apps/web/demos/hover/page.tsx
@@ -22,8 +22,8 @@ function HoverContainer(props: { children: React.ReactNode }) {
   )
 }
 
-function Link(props: { href: string; children: React.ReactNode }) {
-  if (props.href.startsWith("hover:")) {
+function Link(props: { href?: string; children?: React.ReactNode }) {
+  if (props.href?.startsWith("hover:")) {
     const hover = props.href.slice("hover:".length)
     return (
       <span
