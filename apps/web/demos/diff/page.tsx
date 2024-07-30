f6666dac9d510e141773c3810f2d452a7f48c7dd
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-07-30 17:52:46 +0200

    Diff example

--- apps/web/demos/diff/page.tsx
+++ apps/web/demos/diff/page.tsx
@@ -4,6 +4,7 @@ import {
   highlight,
   AnnotationHandler,
   InnerLine,
+  BlockAnnotation,
 } from "codehike/code"
 import Content from "./content.md"
 
@@ -11,29 +12,46 @@ export default function Page() {
   return <Content components={{ Code }} />
 }
 
-const styles = {
-  ins: { borderColor: "#3fb9504d", background: "#2ea04326", icon: "+" },
-  del: { borderColor: "#f851494d", background: "#f8514926", icon: "-" },
-  none: { borderColor: "transparent" },
-} as any
-
-export const diff: AnnotationHandler = {
+const diff: AnnotationHandler = {
   name: "diff",
   onlyIfAnnotated: true,
+  transform: (annotation: BlockAnnotation) => {
+    const color = annotation.query == "-" ? "#f85149" : "#3fb950"
+    return [
+      annotation,
+      {
+        ...annotation,
+        name: "mark",
+        query: color,
+      },
+    ]
+  },
   Line: ({ annotation, ...props }) => {
-    const query = annotation?.query || "none"
-    const { background, borderColor, icon } = styles[query]
     return (
-      <div className="table-row" style={{ background }}>
-        <div
-          className="min-w-[1ch] box-content !opacity-80 pl-2 select-none table-cell border-l-2 "
-          style={{ borderColor }}
-        >
-          {icon}
+      <>
+        <div className="min-w-[1ch] box-content opacity-70 pl-2 select-none">
+          {annotation?.query}
         </div>
-        <div className="table-cell px-2">
-          <InnerLine merge={props} />
-        </div>
+        <InnerLine merge={props} />
+      </>
+    )
+  },
+}
+
+const mark: AnnotationHandler = {
+  name: "mark",
+  Line: ({ annotation, ...props }) => {
+    const color = annotation?.query || "rgb(14 165 233)"
+    return (
+      <div
+        style={{
+          borderLeft: "solid 2px transparent",
+          borderLeftColor: annotation && color,
+          backgroundColor: annotation && `rgb(from ${color} r g b / 0.2)`,
+        }}
+        className="flex"
+      >
+        <InnerLine merge={props} className="px-2 flex-1" />
       </div>
     )
   },
@@ -43,9 +61,9 @@ async function Code({ codeblock }: { codeblock: RawCode }) {
   const highlighted = await highlight(codeblock, "github-dark")
   return (
     <Pre
-      className="m-0 px-0 bg-zinc-950"
+      className="m-0 px-0 bg-zinc-950/90"
       code={highlighted}
-      handlers={[diff]}
+      handlers={[mark, diff]}
     />
   )
 }
