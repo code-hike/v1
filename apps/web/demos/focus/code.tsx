a195d97d5f4305436fa6974ffcd903b0f2e85fd0
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-08-08 14:43:26 +0200

    Add focus implementation

--- apps/web/demos/focus/code.tsx
+++ apps/web/demos/focus/code.tsx
@@ -16,39 +16,6 @@ const ranges = {
   dolor: { fromLineNumber: 11, toLineNumber: 15 },
 }
 
-function useScrollToFocus(ref: React.RefObject<HTMLPreElement>) {
-  const firstRender = useRef(true)
-  useLayoutEffect(() => {
-    if (ref.current) {
-      // find all descendants whith data-focus="true"
-      const focusedElements = ref.current.querySelectorAll(
-        "[data-focus=true]",
-      ) as NodeListOf<HTMLElement>
-
-      // container rect
-      const containerRect = ref.current.getBoundingClientRect()
-
-      // find top and bottom of the focused elements
-      let top = Infinity
-      let bottom = -Infinity
-      focusedElements.forEach((el) => {
-        const rect = el.getBoundingClientRect()
-        top = Math.min(top, rect.top - containerRect.top)
-        bottom = Math.max(bottom, rect.bottom - containerRect.top)
-      })
-
-      // scroll to the focused elements if any part of them is not visible
-      if (bottom > containerRect.height || top < 0) {
-        ref.current.scrollTo({
-          top: ref.current.scrollTop + top - 10,
-          behavior: firstRender.current ? "instant" : "smooth",
-        })
-      }
-      firstRender.current = false
-    }
-  })
-}
-
 export function CodeContainer({ code }: { code: HighlightedCode }) {
   const [focused, setFocused] = useState<"lorem" | "ipsum" | "dolor">("dolor")
 
@@ -107,4 +74,35 @@ const focus: AnnotationHandler = {
   AnnotatedLine: ({ annotation, ...props }) => (
     <InnerLine merge={props} data-focus={true} className="bg-zinc-700/30" />
   ),
+}
+
+function useScrollToFocus(ref: React.RefObject<HTMLPreElement>) {
+  const firstRender = useRef(true)
+  useLayoutEffect(() => {
+    if (ref.current) {
+      // find all descendants whith data-focus="true"
+      const focusedElements = ref.current.querySelectorAll(
+        "[data-focus=true]",
+      ) as NodeListOf<HTMLElement>
+
+      // find top and bottom of the focused elements
+      const containerRect = ref.current.getBoundingClientRect()
+      let top = Infinity
+      let bottom = -Infinity
+      focusedElements.forEach((el) => {
+        const rect = el.getBoundingClientRect()
+        top = Math.min(top, rect.top - containerRect.top)
+        bottom = Math.max(bottom, rect.bottom - containerRect.top)
+      })
+
+      // scroll to the focused elements if any part of them is not visible
+      if (bottom > containerRect.height || top < 0) {
+        ref.current.scrollTo({
+          top: ref.current.scrollTop + top - 10,
+          behavior: firstRender.current ? "instant" : "smooth",
+        })
+      }
+      firstRender.current = false
+    }
+  })
 }
