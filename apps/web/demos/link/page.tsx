393f78c1535176122422e8ab74f0fad4ab15fd8c
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-08-09 09:27:04 +0200

    Link implementation

--- apps/web/demos/link/page.tsx
+++ apps/web/demos/link/page.tsx
@@ -6,8 +6,10 @@ export default function Page() {
 }
 
 async function Code({ codeblock }: { codeblock: RawCode }) {
-  const info = await highlight(codeblock, "github-dark")
-  return <Pre code={info} handlers={[link]} className="m-0 bg-zinc-950/80" />
+  const highlighted = await highlight(codeblock, "github-dark")
+  return (
+    <Pre code={highlighted} handlers={[link]} className="m-0 bg-zinc-950/80" />
+  )
 }
 
 const link: AnnotationHandler = {
